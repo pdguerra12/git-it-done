13c16702026b4c35be9250e0336d46ff90a2834f
Paul Guerra <paulguerra@Pauls-Air.home>
2021-12-12 17:01:35 -0600

    feature/languages completed

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,13 +3,14 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
-    // grab repo name from url query string 
+  // grab repo name from url query string 
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    
+
     if (repoName) {
         // display repo name on the page
         repoNameEl.textContent = repoName;
+
         getReposIssues(repoName);
     } else {
         //if no repo was given, redirect to the homepage
@@ -19,45 +20,47 @@ var getRepoName = function() {
 
 var getReposIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    // make a get request to url
+
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
-                // pass response data to dom function
-                displayIssues(data);
-        
-                // check if api has paginated issues
-                if (response.headers.get("Link")) {
-                    displayWarning(repo);
-                }
-            });
+        response.json().then(function(data) {
+            // pass response data to dom function
+            displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+            displayWarning(repo);
+            }
+        });
         } else {
-            // if not successful, redirect to homepage
-            document.location.replace("./index.html");
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
     });
 };
 
 var displayWarning = function(repo) {
-    // add text to warning container
+    // add text to warning container 
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
-
+    
+    // creates the link element
     var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.textContent = "See More Issues on Github.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
 
     // append to warning container
-    limitWarningEl.appendChild(linkEl);
+    limitWarningEl.appendChild(linkEl)
 };
+
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
-    
+
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -65,19 +68,20 @@ var displayIssues = function(issues) {
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
 
-        // create span to hold issue title
         var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
-        // append to container
+
+        // append to container 
         issueEl.appendChild(titleEl);
 
-        // create a type element
+        // create a type element 
         var typeEl = document.createElement("span");
+
         // check if issue is an actual issue or a pull request
         if (issues[i].pull_request) {
-            typeEl.textContent = "(Pull request)";
+        typeEl.textContent = "(Pull request)";
         } else {
-            typeEl.textContent = "(Issue)";
+        typeEl.textContent = "(Issue)";
         }
 
         // append to container
